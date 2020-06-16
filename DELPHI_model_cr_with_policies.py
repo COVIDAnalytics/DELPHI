30c23761d952f0cd2eef775046a38fbf1c5ae99d
htazi <htazi@mit.edu>
2020-06-16 19:02:12 -0400

    changed file naming & update CR+3.0 06/15

--- DELPHI_model_cr_with_policies.py
+++ DELPHI_model_cr_with_policies.py
@@ -34,8 +34,8 @@ with open("config.yml", "r") as ymlfile:
     CONFIG = yaml.load(ymlfile, Loader=yaml.BaseLoader)
 CONFIG_FILEPATHS = CONFIG["filepaths"]
 USER_RUNNING = "hamza"
-training_start_date = "2020-06-13"  # First date that will be considered for Parameters_Global_<date>.csv
-training_end_date = "2020-06-14"  # First date that excluded for Parameters_Global_<date>.csv
+training_start_date = "2020-06-14"  # First date that will be considered for Parameters_Global_<date>.csv
+training_end_date = "2020-06-15"  # First date that excluded for Parameters_Global_<date>.csv
 max_days_before = (pd.to_datetime(training_end_date) - pd.to_datetime(training_start_date)).days
 time_start = datetime.now()
 
@@ -1069,10 +1069,10 @@ if __name__ == "__main__":
 
         # Dataset with history of previous policy changes
         POLICY_TRACKING_ALREADY_EXISTS = False
-        if os.path.exists(PATH_TO_DATA_SANDBOX + f"policy_change_tracking_world_updated_final_multiproc_0612.csv"):
+        if os.path.exists(PATH_TO_DATA_SANDBOX + f"policy_change_tracking_world_updated_final.csv"):
             POLICY_TRACKING_ALREADY_EXISTS = True
             df_policy_change_tracking_world = pd.read_csv(
-                PATH_TO_DATA_SANDBOX + f"policy_change_tracking_world_updated_final_multiproc_0612.csv"
+                PATH_TO_DATA_SANDBOX + f"policy_change_tracking_world_updated_final.csv"
             )
         else:
             dict_policy_change_tracking_world = {
