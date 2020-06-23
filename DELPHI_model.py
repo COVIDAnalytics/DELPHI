c826c93ad90a3b9c366cf875f1be96b1ea0ab18b
ali <alihaddad_B@bitbucket.org>
2020-06-23 14:03:48 -0400

    Dynideas branch update

--- DELPHI_model.py
+++ DELPHI_model.py
@@ -18,8 +18,10 @@ import yaml
 with open("config.yml", "r") as ymlfile:
     CONFIG = yaml.load(ymlfile, Loader=yaml.BaseLoader)
 CONFIG_FILEPATHS = CONFIG["filepaths"]
-USER_RUNNING = "hamza"
-yesterday = "".join(str(datetime.now().date() - timedelta(days=1)).split("-"))
+USER_RUNNING = "server"
+today_date = datetime.now()
+
+yesterday = "".join(str(today_date.date() - timedelta(days=1)).split("-"))
 # TODO: Find a way to make these paths automatic, whoever the user is...
 PATH_TO_FOLDER_DANGER_MAP = CONFIG_FILEPATHS["danger_map"][USER_RUNNING]
 PATH_TO_WEBSITE_PREDICTED = CONFIG_FILEPATHS["website"][USER_RUNNING]
@@ -267,7 +269,7 @@ for continent, country, province in zip(
 
 # Appending parameters, aggregations per country, per continent, and for the world
 # for predictions today & since 100
-today_date_str = "".join(str(datetime.now().date()).split("-"))
+today_date_str = "".join(str(today_date.date()).split("-"))
 df_global_parameters = pd.concat(list_df_global_parameters)
 df_global_predictions_since_today = pd.concat(list_df_global_predictions_since_today)
 df_global_predictions_since_today = DELPHIAggregations.append_all_aggregations(
