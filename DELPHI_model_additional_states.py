f022053686a8bbab1e6fb9a15624543b502e96b5
ali <alihaddad_B@bitbucket.org>
2020-07-28 11:15:07 -0400

    updated the date from 7/21 to 7/25

--- DELPHI_model_additional_states.py
+++ DELPHI_model_additional_states.py
@@ -25,8 +25,8 @@ with open("config.yml", "r") as ymlfile:
     CONFIG = yaml.load(ymlfile, Loader=yaml.BaseLoader)
 CONFIG_FILEPATHS = CONFIG["filepaths"]
 USER_RUNNING = "server"
-training_start_date = datetime(2020, 7, 9)
-training_end_date = datetime(2020, 7, 21)
+training_start_date = datetime(2020, 7, 21)
+training_end_date = datetime(2020, 7, 26)
 training_last_date = training_end_date - timedelta(days=1)
 # Default training_last_date is up to day before now, but depends on what's the most recent historical data you have
 n_days_to_train = (training_last_date - training_start_date).days
