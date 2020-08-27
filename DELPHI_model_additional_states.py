419b0bfb18a395f03005a93525294914e476b3a5
ali <alihaddad_B@bitbucket.org>
2020-08-27 17:48:42 -0400

    new datasets

--- DELPHI_model_additional_states.py
+++ DELPHI_model_additional_states.py
@@ -35,7 +35,7 @@ with open("config.yml", "r") as ymlfile:
 CONFIG_FILEPATHS = CONFIG["filepaths"]
 USER_RUNNING = "server"
 training_start_date = datetime(2020, 8, 3)
-training_end_date = datetime(2020, 8, 19)
+training_end_date = datetime(2020, 8, 26)
 training_last_date = training_end_date - timedelta(days=1)
 # Default training_last_date is up to day before now, but depends on what's the most recent historical data you have
 n_days_to_train = (training_last_date - training_start_date).days
@@ -72,15 +72,19 @@ def solve_and_predict_area_additional_states(
         PATH_TO_DATA_SANDBOX + f"processed/Ex_US_counties.csv"
     )
 
-    if country_sub not in ["US"]: #, "Colombia", "Mexico", "Argentina", "Chile", "Peru", "Brazil"]:
+    ex_us_regions = pd.read_csv(
+        PATH_TO_DATA_SANDBOX + f"processed/Ex_US_regions.csv"
+    )
+
+    if country_sub not in ["US","Argentina", "Brazil", "Chile", "Colombia",
+                          "South_Africa", "Mexico", "Peru", "Italy", "Spain", "Canada", "Australia"]: #, "Colombia", "Mexico", "Argentina", "Chile", "Peru", "Brazil"]:
         return None
     elif country_sub == "US":
         if province_sub not in us_county_names.Province.values:
             return None
-    elif country_sub in ["Argentina", "Brazil", "Chile", "Colombia", "Russia", "South_Africa", "Mexico", "Peru", "Italy", "Spain"]:
-        if province_sub == "None":
-            return None
-        elif province_sub not in ex_us_county_names[ex_us_county_names.Country == country_sub].Province.values:
+    elif country_sub != "US" :
+        if province_sub == "None" or province_sub not in ex_us_county_names[ex_us_county_names.Country == country_sub].Province.values and \
+                province_sub not in ex_us_regions[ex_us_regions.Country == country_sub].Province.values :
             return None
 
     if current_parameters_ is not None:
