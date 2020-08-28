23aeb2d7e075f0df90d4ae28fe239c69ad2d7a9b
ali <alihaddad_B@bitbucket.org>
2020-08-28 08:43:33 -0400

    fixing ex-us regions

--- DELPHI_model_additional_states.py
+++ DELPHI_model_additional_states.py
@@ -34,7 +34,7 @@ with open("config.yml", "r") as ymlfile:
     CONFIG = yaml.load(ymlfile, Loader=yaml.BaseLoader)
 CONFIG_FILEPATHS = CONFIG["filepaths"]
 USER_RUNNING = "server"
-training_start_date = datetime(2020, 8, 3)
+training_start_date = datetime(2020, 8, 21)
 training_end_date = datetime(2020, 8, 26)
 training_last_date = training_end_date - timedelta(days=1)
 # Default training_last_date is up to day before now, but depends on what's the most recent historical data you have
@@ -76,16 +76,20 @@ def solve_and_predict_area_additional_states(
         PATH_TO_DATA_SANDBOX + f"processed/Ex_US_regions.csv"
     )
 
-    if country_sub not in ["US","Argentina", "Brazil", "Chile", "Colombia",
-                          "South_Africa", "Mexico", "Peru", "Italy", "Spain", "Canada", "Australia"]: #, "Colombia", "Mexico", "Argentina", "Chile", "Peru", "Brazil"]:
+    # if country_sub not in ["US","Argentina", "Brazil", "Chile", "Colombia",
+    #                        "South_Africa", "Mexico", "Peru", "Italy", "Spain", "Canada", "Australia"]: #, "Colombia", "Mexico", "Argentina", "Chile", "Peru", "Brazil"]:
+    if country_sub not in [ "Brazil", "Chile", "Colombia",
+                           "Mexico", "Peru", "Spain"]: #, "Colombia", "Mexico", "Argentina", "Chile", "Peru", "Brazil"]:
         return None
     elif country_sub == "US":
         if province_sub not in us_county_names.Province.values:
             return None
     elif country_sub != "US" :
-        if province_sub == "None" or province_sub not in ex_us_county_names[ex_us_county_names.Country == country_sub].Province.values and \
-                province_sub not in ex_us_regions[ex_us_regions.Country == country_sub].Province.values :
+        if province_sub == "None" or province_sub not in ex_us_regions[ex_us_regions.Country == country_sub].Province.values :
             return None
+        # if province_sub == "None" or province_sub not in ex_us_county_names[ex_us_county_names.Country == country_sub].Province.values and \
+        #         province_sub not in ex_us_regions[ex_us_regions.Country == country_sub].Province.values :
+        #     return None
 
     if current_parameters_ is not None:
         current_parameter = current_parameters_[
