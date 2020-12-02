1139316f734f1dc3559ba638d62eb7df85ae691d
ali <alihaddad_B@bitbucket.org>
2020-12-02 16:19:11 -0500

    set up git credentials 2

--- run_model4.sh
+++ run_model4.sh
@@ -9,6 +9,7 @@ git config --global user.name "Server"
 git pull
 git add data_sandbox/predicted/parameters/*
 git add data_sandbox/predicted/policy_scenario_predictions/*
+git add danger_map/predicted/*
 git -c user.name='Server' -c user.email=ali@dynideas.com commit -m "Ex US results"
 git push https://"$GIT_USER":"$GIT_PASS"@github.com/COVIDAnalytics/DELPHI.git --all
 
