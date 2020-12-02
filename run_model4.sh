8b25fac48e9f741462a27def9b888b2529f8aa23
ali <alihaddad_B@bitbucket.org>
2020-12-02 15:41:10 -0500

    set up git credentials

--- run_model4.sh
+++ run_model4.sh
@@ -4,9 +4,11 @@ python archive/V3\ Model/DELPHI_model_V3_predict_additional_state.py -d "2020-07
 python DELPHI_model_V4_additional_states.py -rc run_configs/annealing-run-config.yml -t 'ExUS'
 #python DELPHI_model_V4_additional_states.py -rc run_configs/annealing-run-config.yml -t 'US'
 
+git config --global user.email "ali@dynideas.com"
+git config --global user.name "Server"
 git pull
 git add data_sandbox/predicted/parameters/*
 git add data_sandbox/predicted/policy_scenario_predictions/*
-git commit -m "Ex US results" --author "Server <ali@dynideas.com"
-git push https://$GIT_USER:$GIT_PASS@github.com/COVIDAnalytics/DELPHI.git --all
+git -c user.name='Server' -c user.email=ali@dynideas.com commit -m "Ex US results"
+git push https://"$GIT_USER":"$GIT_PASS"@github.com/COVIDAnalytics/DELPHI.git --all
 
