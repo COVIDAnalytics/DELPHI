a4cdab18eda9cbdb6f1e4992357ee3b417263a4c
Server <ali@dynideas.com>
2020-12-03 11:22:58 -0500

    updated global 2

--- run_model_V4_global.sh
+++ run_model_V4_global.sh
@@ -1,14 +1,13 @@
 git config --global user.email "ali@dynideas.com"
 git config --global user.name "Server"
-cd ..
+cd ..;cd ..;cd mnt
 git clone https://"$GIT_USER":"$GIT_PASS"@github.com/COVIDAnalytics/covid19orc.git
 cd covid19orc/
 git checkout danger_map
 cd ..
-'cp' -rf covid19orc/danger_map/processed/* DELPHI/danger_map/processed/
-'cp' -rf covid19orc/danger_map/predicted/* DELPHI/danger_map/predicted/
-rm -rf covid19orc
-cd DELPHI
+'cp' -rf covid19orc/danger_map/processed/* /repos/DELPHI/danger_map/processed/
+'cp' -rf covid19orc/danger_map/predicted/* /repos/DELPHI/danger_map/predicted/
+rm -rf covid19orc;cd ..;cd /repos/DELPHI
 #python archive/V3\ Model/DELPHI_model_V3_predict_additional_state.py -d "2020-07-01" -t 'global'
 
 python DELPHI_model_V4_additional_states.py -rc run_configs/annealing-run-config.yml -t 'global'
