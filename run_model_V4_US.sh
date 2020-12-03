f0d33bb119f6df81b0179606de3a8131990c4380
Server <ali@dynideas.com>
2020-12-03 16:11:32 -0500

    correct the source file

--- run_model_V4_US.sh
+++ run_model_V4_US.sh
@@ -1,8 +1,10 @@
+git config --global user.email "ali@dynideas.com"; git config --global user.name "Server" ;
+cd ..;cd ..;cd /repos/DELPHI
 python archive/V3\ Model/DELPHI_model_V3_predict_additional_state.py -d "2020-07-01" -t 'US'
 
 python DELPHI_model_V4_additional_states.py -rc run_configs/annealing-run-config.yml -t 'US'
 
-git config --global user.email "ali@dynideas.com";git config --global user.name "Server";git pull
+git pull
 git add data_sandbox/processed/* ; git add data_sandbox/raw_data_additional_states/* ; git add data_sandbox/predicted/parameters/*
 git add data_sandbox/predicted/policy_scenario_predictions/*; git add logs/*
 git -c user.name='Server' -c user.email=ali@dynideas.com commit -m "US results"
