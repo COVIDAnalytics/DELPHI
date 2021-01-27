2918f4521426cee3144cda2e3f33d0e9e786fbeb
Server <ali@dynideas.com>
2021-01-27 18:05:36 -0500

    return back the scripts

--- run_model_V4_ExUS_oneWeekAgo.sh
+++ run_model_V4_ExUS_oneWeekAgo.sh
@@ -1,9 +1,10 @@
 git config --global user.email "ali@dynideas.com"; git config --global user.name "Server" ;
 cd ..;cd ..;cd /repos/DELPHI
-python DELPHI_model_V3_predict_additional_state.py -d "2020-07-01" -t 'ExUS' -g 'false' -dt '012421'
+python DELPHI_model_V3_predict_additional_state.py -d "2020-07-01" -t 'ExUS' -g 'false'
 
-python DELPHI_model_V4_additional_states.py -rc run_configs/annealing-run-config.yml -t 'ExUS' -g 'false' -weekago 'true' -d '012421'
+python DELPHI_model_V4_additional_states.py -rc run_configs/annealing-run-config.yml -t 'ExUS' -g 'false' -weekago 'true'
 
+git stash
 git pull
 git add data_sandbox/predicted/policy_scenario_predictions/* ;git add logs/*
 git -c user.name='Server' -c user.email=ali@dynideas.com commit -m "Ex US a week ago results"
