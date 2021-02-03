239e5a44132966734bde2efad4bbdbca2f448878
Server <ali@dynideas.com>
2021-02-03 17:36:18 -0500

    temporary , roll back after running

--- run_model_V4_ExUS.sh
+++ run_model_V4_ExUS.sh
@@ -1,8 +1,8 @@
 git config --global user.email "ali@dynideas.com"; git config --global user.name "Server" ;
 cd ..;cd ..;cd /repos/DELPHI
-python DELPHI_model_V3_predict_additional_state.py -d "2020-07-01" -t 'ExUS' -g 'false'
+python DELPHI_model_V3_predict_additional_state.py -d "2020-07-01" -t 'ExUS' -g 'false'  -dt '013121'
 
-python DELPHI_model_V4_additional_states.py -rc run_configs/annealing-run-config.yml -t 'ExUS' -g 'false'
+python DELPHI_model_V4_additional_states.py -rc run_configs/annealing-run-config.yml -t 'ExUS' -g 'false'  -d '013121'
 
 git pull
 git add data_sandbox/processed/* ;git add data_sandbox/raw_data_additional_states/* ;git add data_sandbox/predicted/raw_predictions/*
