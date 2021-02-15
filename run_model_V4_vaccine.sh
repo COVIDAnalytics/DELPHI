db8b6266f1dd9b4512af86dce9f45b66f48460ec
Server <ali@dynideas.com>
2021-02-15 15:05:23 -0500

    update the script

--- run_model_V4_vaccine.sh
+++ run_model_V4_vaccine.sh
@@ -1,6 +1,6 @@
 git config --global user.email "ali@dynideas.com"; git config --global user.name "Server" ;
 cd ..;cd ..;cd /repos/DELPHI
-#python DELPHI_model_V3_predict_additional_state.py -d "2020-07-01" -t 'global'
+#python DELPHI_model_V3_predict_additional_state.py -d "2020-10-01" -t 'global'
 python DELPHI_model_V4.py -rc run_configs/annealing-run-config.yml
 
 python DELPHI_model_V4_additional_states.py -rc run_configs/annealing-run-config.yml -t 'global_vaccine'
