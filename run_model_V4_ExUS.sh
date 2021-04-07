fc3063e644b107979fa6a22828b8e56a9de8cd02
Server <ali@dynideas.com>
2021-04-07 13:20:22 -0400

    temporary date in ExUS US oneWeekAgo

--- run_model_V4_ExUS.sh
+++ run_model_V4_ExUS.sh
@@ -1,20 +1,21 @@
 git config --global user.email "ali@dynideas.com"; git config --global user.name "Server" ;
 cd ..;cd ..;cd /repos/DELPHI
-python DELPHI_model_V3_predict_additional_state.py -d "2020-10-01" -t 'ExUS' -g 'false'
+python DELPHI_model_V3_predict_additional_state.py -d "2020-10-01" -t 'ExUS' -g 'false' -dt '040621'
 
-python DELPHI_model_V4_additional_states.py -rc run_configs/annealing-run-config.yml -t 'ExUS' -g 'false'
+python DELPHI_model_V4_additional_states.py -rc run_configs/annealing-run-config.yml -t 'ExUS' -g 'false' -d '040621'
 
 git stash -- data_sandbox/predicted/raw_predictions/*
 git stash -- data_sandbox/predicted/policy_scenario_predictions/*
 git stash -- data_sandbox/processed/*
 git stash -- data_sandbox/raw_data_additional_states/*
 git stash -- danger_map/*
+git stash -- logs/*
 
 
 git pull
 
 git add data_sandbox/predicted/parameters/*
-git add data_sandbox/predicted/policy_scenario_predictions/*; git add logs/*
+git add data_sandbox/predicted/policy_scenario_predictions/*
 git -c user.name='Server' -c user.email=ali@dynideas.com commit -m "Ex US results"
 git push https://"$GIT_USER":"$GIT_PASS"@github.com/COVIDAnalytics/DELPHI.git --all
 
