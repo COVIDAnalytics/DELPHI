42a970a86e852b9d5fef21a784144a4865481469
Server <ali@dynideas.com>
2021-04-12 11:01:42 -0400

    fixed the vacination function method

--- run_model_V4_vaccine.sh
+++ run_model_V4_vaccine.sh
@@ -1,5 +1,12 @@
 git config --global user.email "ali@dynideas.com"; git config --global user.name "Server" ;
-cd ..;cd ..;cd /repos/DELPHI
+cd ..;cd ..;cd mnt
+git clone https://"$GIT_USER":"$GIT_PASS"@github.com/COVIDAnalytics/covid19orc.git
+cd covid19orc/
+git checkout danger_map
+cd ..
+'cp' -rf covid19orc/danger_map/processed/* /repos/DELPHI/danger_map/processed/
+'cp' -rf covid19orc/danger_map/predicted/* /repos/DELPHI/danger_map/predicted/
+rm -rf covid19orc;cd ..;cd /repos/DELPHI
 #python DELPHI_model_V3_predict_additional_state.py -d "2020-10-01" -t 'global'
 python DELPHI_model_V4.py -rc run_configs/annealing-run-config.yml
 
