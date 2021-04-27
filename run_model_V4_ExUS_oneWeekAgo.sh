502ea13594b76948ca95cd488ac2f1a814e0ea31
Server <ali@dynideas.com>
2021-04-27 17:36:33 -0400

    updated script

--- run_model_V4_ExUS_oneWeekAgo.sh
+++ run_model_V4_ExUS_oneWeekAgo.sh
@@ -4,9 +4,9 @@ python DELPHI_model_V3_predict_additional_state.py -d "2020-10-01" -t 'ExUS' -g 
 
 python DELPHI_model_V4_additional_states.py -rc run_configs/annealing-run-config.yml -t 'ExUS' -g 'false' -weekago 'true'
 
-git stash
-git pull
-#git add data_sandbox/predicted/policy_scenario_predictions/*
-git -c user.name='Server' -c user.email=ali@dynideas.com commit -m "Ex US a week ago results"
-git push https://"$GIT_USER":"$GIT_PASS"@github.com/COVIDAnalytics/DELPHI.git --all
-
+#git stash
+#git pull
+##git add data_sandbox/predicted/policy_scenario_predictions/*
+#git -c user.name='Server' -c user.email=ali@dynideas.com commit -m "Ex US a week ago results"
+#git push https://"$GIT_USER":"$GIT_PASS"@github.com/COVIDAnalytics/DELPHI.git --all
+#
